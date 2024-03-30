76b797a64dd30f14bde3f28bfe76bb6876e82877
adamd <adam.d.hinton@gmail.com>
2024-03-30 12:11:59 -0400

    my first initializer block
    Runs any code you want, as many as you want, before constructor code runs
    
    This blew my mind.

--- src/dev/lpa/Parent.java
+++ src/dev/lpa/Parent.java
@@ -2,24 +2,29 @@ package dev.lpa;
 
 public class Parent {
 
-    private String name;
-    private String dob;
+    private final String name;
+    private final String dob;
+
+
+    // This is an instance initializer block
+    // Declared directly in a class body
+    // Executed when instance is created
+    // Executed before any code in class constructors is executed
+    // can have multiple blocks, exec in order declared
+
+    {
+        name = "John Doe";
+        dob = "01/01/1900";
+    }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getDob() {
         return dob;
     }
 
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
 
     @Override
     public String toString() {
