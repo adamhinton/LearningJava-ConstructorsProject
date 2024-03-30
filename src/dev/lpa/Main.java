32891aad25d774bbd0c94b7b93b53183d08cc742
adamd <adam.d.hinton@gmail.com>
2024-03-30 12:07:25 -0400

    Flesh out Parent
    Notes: CLasses don't need constructor. They have implicit noargs constructors otherwise.

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,10 +1,14 @@
 package dev.lpa;
 
+import external.Child;
+
 public class Main {
     public static void main(String[] args) {
 
+        // RIght now Parent has no constructor
+        // Don't actually need to write a constructor to instantiate a class
 
         Parent parent = new Parent();
-
+        Child child = new Child();
     }
 }
