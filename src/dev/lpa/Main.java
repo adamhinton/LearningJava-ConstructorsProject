0fb455eea36769f565a9841f15e282c2a29938c2
adamd <adam.d.hinton@gmail.com>
2024-03-31 10:52:52 -0400

    My first enum constructors

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,17 +1,26 @@
 package dev.lpa;
 
-import external.Child;
-
 public class Main {
     public static void main(String[] args) {
 
         // RIght now Parent has no constructor
         // Don't actually need to write a constructor to instantiate a class
 
-        Parent parent = new Parent("Jane Doe", "01/01/1950", 4);
-        Child child = new Child();
+//        Parent parent = new Parent("Jane Doe", "01/01/1950", 4);
+//        Child child = new Child();
+//
+//        System.out.println("Parent: " + parent);
+//        System.out.println("Child: " + child);
+//
+//
+//        Person joe = new Person("Joe", "01-01-1950");
+//        System.out.println(joe);
+//
+//
+//        Person joeCopy = new Person(joe);
+//        System.out.println(joeCopy);
 
-        System.out.println("Parent: " + parent);
-        System.out.println("Child: " + child);
+        Generation g = Generation.BABY_BOOMER;
+
     }
 }
--- src/dev/lpa/Person.java
+++ src/dev/lpa/Person.java
@@ -1,6 +1,5 @@
 package dev.lpa;
 
-public record Person(String name, String dob) {
     // Constructor auto-made
         // Called canonical constructor
 
@@ -21,4 +20,26 @@ public record Person(String name, String dob) {
 
 
 
+public record Person(String name, String dob) {
+
+    // DOn't have to gen canonical ctr, but we can if we want to do transfxns etc
+//    public Person(String name, String dob) {
+//        this.name = name;
+//        this.dob = dob.replace('-', '/');
+//    }
+
+    // copy ctr
+    public Person(Person p) {
+        this(p.name, p.dob);
+    }
+
+    // Missing parens
+    // implicit canonical ctr
+    // Can focus on just the custom bit of code
+    public Person {
+        // has access to args of cncl ctr, like name and dob
+        if (dob == null) throw new IllegalArgumentException("Bad data");
+        dob = dob.replace('-', '/');
+
+    }
 }
